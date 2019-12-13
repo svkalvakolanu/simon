0d1c0f8aeb308a0414ba92e88dec49f9af22910d
svkalvakolanu <skalvakolanu@gmail.com>
2019-12-13 13:24:44 -0500

    color randomizer code snip built

---
+++ colors.js
@@ -0,0 +1,48 @@
+//idea from: https://bost.ocks.org/mike/shuffle/
+
+let colors = [];
+
+let colorSet = [];
+let functionArray = [];
+
+let color1 = Math.floor(Math.random() * 256);
+let color2 = Math.floor(Math.random() * 221);
+let color3 = Math.floor(Math.random() * 221);
+
+function buildColorArray(num) {
+  for (i = 0; i < num; i++) {
+    colorSet = [];
+    color1 = Math.floor(Math.random() * 256);
+    color2 = Math.floor(Math.random() * 221);
+    color3 = Math.floor(Math.random() * 221);
+
+    functionArray = [color1, color2, color3];
+
+    console.log(functionArray);
+    colorSet = shuff(functionArray);
+
+    console.log(colorSet);
+
+    colors.push(colorSet);
+  }
+}
+
+function shuff(arr) {
+  let idx = arr.length,
+    hold,
+    ridx;
+
+  // While there remain elements to shuffle...
+  while (0 !== idx) {
+    // Pick a remaining element...
+    ridx = Math.floor(Math.random() * idx);
+    idx -= 1;
+
+    // And swap it with the current element.
+    hold = arr[idx];
+    arr[idx] = arr[ridx];
+    arr[ridx] = hold;
+  }
+
+  return arr;
+}
